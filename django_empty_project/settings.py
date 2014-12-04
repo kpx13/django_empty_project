0ff45bec87b58f66f4153d80cacfb24dfff45071
annkpx <annkpx@gmail.com>
2014-12-04 14:51:15 -0500

    fix

--- django_empty_project/settings.py
+++ django_empty_project/settings.py
@@ -12,7 +12,6 @@ https://docs.djangoproject.com/en/1.7/ref/settings/
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
-
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
@@ -85,3 +84,5 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.7/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = STATICFILES_ROOT = os.path.join(BASE_DIR, 'static')
+
